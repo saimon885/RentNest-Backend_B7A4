9ae99111a7e8002add21a776902e665fbeab2fda
saimon885 <saimonhossan34567@gmail.com>
2026-07-07 17:15:32 +0600

    admin properties,rentals,states

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -10,10 +10,32 @@ const getAllUser = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
-const getAllProperties = catchAsync((req: Request, res: Response) => {});
-const getAllRentalRequest = catchAsync((req: Request, res: Response) => {});
+
+const getAllProperties = catchAsync(async (req: Request, res: Response) => {
+  const result = await adminService.getAllProperties();
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "All Property retrieved successfully",
+    data: result,
+  });
+});
+const getAllRentalRequest = catchAsync(async (req: Request, res: Response) => {
+  const result = await adminService.getAllRentals();
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "All rental Request retrieved successfully",
+    data: result,
+  });
+});
 // advanced
-const getAllStates = catchAsync((req: Request, res: Response) => {});
+const getAllStates = catchAsync(async (req: Request, res: Response) => {
+  const result = await adminService.getAllStates();
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "All States retrieved successfully",
+    data: result,
+  });
+});
 
 const updateUser = catchAsync((req: Request, res: Response) => {});
 
--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -8,9 +8,57 @@ const getAllUserDB = async () => {
   });
   return result;
 };
-const getAllProperties = () => {};
-const getAllRentals = () => {};
-const getAllStates = () => {};
+const getAllProperties = async () => {
+  const result = await prisma.property.findMany();
+  return result;
+};
+const getAllRentals = async () => {
+  const result = await prisma.rentalRequest.findMany();
+  return result;
+};
+const getAllStates = async () => {
+  const transactionResult = await prisma.$transaction(async (tx) => {
+    const [
+      totalUsers,
+      TotalAdmin,
+      TotalLandLord,
+      TotalTenant,
+      totalProperty,
+      totalCategory,
+      totalRentalRequest,
+    ] = await Promise.all([
+      await tx.users.count(),
+      await tx.users.count({
+        where: {
+          role: "ADMIN",
+        },
+      }),
+      await tx.users.count({
+        where: {
+          role: "LANDLORD",
+        },
+      }),
+      await tx.users.count({
+        where: {
+          role: "TENANT",
+        },
+      }),
+      await tx.property.count(),
+      await tx.category.count(),
+      await tx.rentalRequest.count(),
+    ]);
+    return {
+      totalUsers,
+      TotalAdmin,
+      TotalLandLord,
+      TotalTenant,
+      totalProperty,
+      totalCategory,
+      totalRentalRequest,
+    };
+  });
+  return transactionResult;
+};
 const updateUserBannedReq = () => {};
 export const adminService = {
   getAllUserDB,
